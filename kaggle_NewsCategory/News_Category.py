b077bb15b8818385ab1f0e097cf2b5ee315d5192
yeseul106 <cho898989@naver.com>
2021-01-29 02:50:18 +0900

    주석처리 추가

--- kaggle_NewsCategory/News_Category.py
+++ kaggle_NewsCategory/News_Category.py
@@ -34,6 +34,7 @@ token.fit_on_texts(news[:, 0])
 newsVal = news[:, 0]
 # print("newsValLen: ", len(newsVal))
 
+#문장을 단어로 쪼개서 단어만 들어있는 리스트로 만들기
 sequenceWord = []
 for i in range(7628):
     sequenceWord.append(text_to_word_sequence(newsVal[i]))
@@ -74,17 +75,17 @@ print(max)  #513
 #패딩, 서로 다른 길이의 데이터를 500으로 맞춤
 x_train = pad_sequences(x_train, maxlen=500)
 x_test = pad_sequences(x_test, maxlen=500)
-#데이터 전처리
+#데이터 전처리 (y를 원-핫 인코딩)
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
 
 word_size = len(token.word_index)+1
 
-# '''NN Model'''
-# model = Sequential()
-# model.add(Embedding(word_size, 200, input_length=500))
-# model.add(Flatten())
-# model.add(Dense(4, activation='softmax'))
+'''NN Model'''
+model = Sequential()
+model.add(Embedding(word_size, 200, input_length=500))
+model.add(Flatten())
+model.add(Dense(4, activation='softmax'))
 
 # '''CNN Model'''
 # model = Sequential()
@@ -102,14 +103,14 @@ word_size = len(token.word_index)+1
 # model.add(LSTM(500, activation='tanh'))
 # model.add(Dense(4, activation='softmax'))
 
-'''LSTM + CNN Model'''
-model = Sequential()
-model.add(Embedding(word_size, 200, input_length=500))
-model.add(Dropout(0.5))
-model.add(Conv1D(200, 3, padding='valid', activation='relu', strides=1))
-model.add(LSTM(55))
-model.add(Dense(4))
-model.add(Activation('sigmoid'))
+# '''LSTM + CNN Model'''
+# model = Sequential()
+# model.add(Embedding(word_size, 200, input_length=500))
+# model.add(Dropout(0.5))
+# model.add(Conv1D(200, 3, padding='valid', activation='relu', strides=1))
+# model.add(LSTM(55))
+# model.add(Dense(4))
+# model.add(Activation('sigmoid'))
 
 '''model compile & fit'''
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
